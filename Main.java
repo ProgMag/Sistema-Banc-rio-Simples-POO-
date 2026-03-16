6a489d986f53ef6af97e2fcfce2754059e947dd3
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-03-16 17:48:06 -0300

    alterando o if de sacar

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ContaCorrente cont = new ContaCorrente();
-        ContaPoupanca cont2 = new ContaPoupanca();
+        ContaCorrente contCorrente = new ContaCorrente();
+        ContaPoupanca contPoupanca = new ContaPoupanca();
         int opcao = 0;
 
         while (opcao != 4) {
             System.out.println("""
                     0 - Conta corrente
                     2 - Conta poupança
-                    4 - Sair
+                    4 - Sair do banco
                     Digite a opção desejada:
                     """);
             opcao = sc.nextInt();
@@ -25,7 +25,7 @@ public class Main {
                                     1 - Deposito
                                     2 - Saque
                                     3 - Saldo
-                                    4 - Sair
+                                    4 - Sair da conta corrente
                             Digite um numero
                             """);
                     escolha1 = sc.nextInt();
@@ -34,14 +34,19 @@ public class Main {
                         case 1 -> {
                             System.out.println("Qual valor você desejaria depositar");
                             double valorDeposito = sc.nextInt();
-                            cont.depositar(valorDeposito);
+                            contCorrente.depositar(valorDeposito);
                         }
                         case 2 -> {
                             System.out.println("Qual valor você desejaria sacar");
                             double valorSacar = sc.nextDouble();
-                            cont.sacar(valorSacar);
+                            if (valorSacar > contCorrente.getSaldo()) {
+                                System.out.println("Não será possível sacar " + valorSacar
+                                        + " reais.\n O valor é maior do que seu saldo: " + contCorrente.getSaldo());
+                            } else {
+                                contCorrente.sacar(valorSacar);
+                            }
                         }
-                        case 3 -> System.out.println(cont.getSaldo());
+                        case 3 -> System.out.println(contCorrente.getSaldo());
                         case 4 -> System.out.println("Encerrando conta corrente");
                     }
                 }
@@ -55,7 +60,7 @@ public class Main {
                                     1 - Deposito
                                     2 - Saque
                                     3 - Saldo
-                                    4 - Sair
+                                    4 - Sair da conta poupança
                                 Digite um numero
                             """);
                     escolha2 = sc.nextInt();
@@ -63,15 +68,20 @@ public class Main {
                         case 1 -> {
                             System.out.println("Qual valor você desejaria depositar");
                             double valorDeposito = sc.nextInt();
-                            cont2.depositar(valorDeposito);
+                            contPoupanca.depositar(valorDeposito);
                         }
                         case 2 -> {
                             System.out.println("Qual valor você desejaria sacar");
                             double valorSacar = sc.nextDouble();
-                            cont2.sacar(valorSacar);
+                            if (valorSacar > contCorrente.getSaldo()) {
+                                System.out.println("Não será possível sacar " + valorSacar
+                                        + " reais.\n O valor é maior do que seu saldo: " + contPoupanca.getSaldo());
+                            } else {
+                                 contPoupanca.sacar(valorSacar);
+                            }
                         }
-                        case 3 -> System.out.println(cont2.getSaldo());
-                        case 4 -> System.out.println("Encerrando conta poupança");
+                        case 3 -> System.out.println(contPoupanca.getSaldo());
+                        case 4 -> System.out.println("Encerrando seu conta poupança");
                     }
                 }
             }
